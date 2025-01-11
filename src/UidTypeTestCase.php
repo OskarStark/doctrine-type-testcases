1c9626e45fec8f40297a267331cb97c28e9ce19e
Oskar Stark <oskarstark@googlemail.com>
2025-01-11 12:51:55 +0100

    -

--- src/UidTypeTestCase.php
+++ src/UidTypeTestCase.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of oskarstark/doctrine-type-testcases package.
+ * This file is part of oskarstark/trimmed-non-empty-string.
  *
- * (c) Datana GmbH <info@datana.rocks>
+ * (c) Oskar Stark <oskarstark@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
